6faa6815e58d76d652a14864b3774d2bedef2882
gahyeon <janghyeon2412@gmail.com>
2025-11-09 23:50:46 +0900

    init: company service 프로젝트 생성

---
+++ src/test/java/com/logilink/company/CompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logilink.company;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
